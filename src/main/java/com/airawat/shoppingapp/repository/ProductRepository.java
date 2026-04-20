16b0448ef7fb510f49192f76747fc4d6dc930841
Jayant-129 <agrawaljayant86@gmail.com>
2026-04-20 14:10:30 +0530

    Coded ProductRepository.java for product module

--- src/main/java/com/airawat/shoppingapp/repository/ProductRepository.java
+++ src/main/java/com/airawat/shoppingapp/repository/ProductRepository.java
@@ -2,7 +2,7 @@ package com.airawat.shoppingapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.airawat.shoppingapp.entity.Product;
+import com.airawat.shoppingapp.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsBySku(String sku);
